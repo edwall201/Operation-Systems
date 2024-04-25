1b9cf3dcd86d219670e2a793b0cba83878f10fea
EDWARD-TUF\Edward <edwall201@gmail.com>
2024-04-25 11:43:55 +0800

    fix shell

--- HW1/Programming/ProgrammingProjects/CH3/shell.c
+++ HW1/Programming/ProgrammingProjects/CH3/shell.c
@@ -9,14 +9,11 @@
 
 #define MAX_LINE 80
 
-// Ham lay input
 int getInput(char input[], char history[])
 {
     printf("osh>");
     fflush(stdout);
-    // gets(input);
-    fgets(input, MAX_LINE, stdin); // khong nen dung gets vi tu C11 da bi xoa so roi
-    // xoa dau \n cuoi cung
+    fgets(input, MAX_LINE, stdin); 
     input[strlen(input) - 1] = '\0';
     if (strcmp(input, "!!") == 0)
     {
@@ -40,47 +37,36 @@ int getInput(char input[], char history[])
     return 1;
 }
 
-// Ham execute thong thuong khong dung pipe
 void execArgs(char *args[], int len)
 {
-    // Khởi tạo sự kiện chia tiến trình
     pid_t child_pid;
     int flag = 0;
-    // Kiểm tra phần tử cuối cùng của mảng các token có dấu & hay không?
     if (*args[len - 1] == '&')
-    { // Nghĩa là tiến trinh f và tiến trình con chạy song song
+    {
         flag = 1;
         args[len - 1] = NULL;
         len--;
     }
     child_pid = fork();
-    // Kiểm tra xem tiến trình đang chạy là tiến trình cha hay tiến trình con
     if (child_pid == 0)
-    { // Tiến trình đang chạy là tiến trình con
+    { 
         if (len >= 3)
-        { // Nếu là chuyển hướng output
+        { 
             if (strcmp(args[len - 2], ">") == 0)
             {
                 args[len - 2] = NULL;
-                // Mở file để  ghi vào
                 int out = open(args[len - 1], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-                // Chuyển từ thiết bị xuất chuẩn (màn hình) sang file
                 dup2(out, STDOUT_FILENO);
-                // Đóng file
                 close(out);
             }
             else if (strcmp(args[len - 2], "<") == 0)
-            { // Nếu là chuyển hướng input
+            { 
                 args[len - 2] = NULL;
-                // Mở file input ra
                 int in = open(args[len - 1], O_RDONLY);
-                // Chuyển hướng input từ thiết bị chuyển (keyboard) -> sang lấy từ file
                 dup2(in, STDIN_FILENO);
-                // Đóng file
                 close(in);
             }
         }
-        // Thực thi lệnh
         if (execvp(args[0], args) < 0)
         {
             printf("\nCould not execute in command...");
@@ -88,8 +74,7 @@ void execArgs(char *args[], int len)
         exit(1);
     }
     else if (child_pid > 0)
-    { // Là tiến trình cha
-        // Trường hợp không có dấu &
+    {
         if (flag == 0)
         {
             wait(NULL);
@@ -97,14 +82,13 @@ void execArgs(char *args[], int len)
         return;
     }
     else
-    { // Không thể  chia được tiến trình
+    {
 
         printf("\nCan't fork!!!");
         return;
     }
 }
 
-// Ham parse ra cac doi so
 int parseInput(char *args[], char input[])
 {
     int count = 0;
@@ -118,7 +102,6 @@ int parseInput(char *args[], char input[])
     return count;
 }
 
-// Ham kiem tra doi so truyen vao co dung pipe hay khong? Neu co tra ve vi tri, nguoc lai tra ve -1
 int checkPipe(char *args[], int len)
 {
     for (int i = 0; i < len; i++)
@@ -129,28 +112,23 @@ int checkPipe(char *args[], int len)
     return -1;
 }
 
-// Ham execute dung pipe
 void execArgsPipe(char *args[], char *argsPipe[], int flag)
 {
     pid_t pid;
 
-    int fd[2]; // file decription
+    int fd[2];
     if (pipe(fd) < 0)
-    { // Khởi tạo 1 đường ống dẫn
+    { 
         printf("\nCan't init pipe!!!!");
         return;
     }
-    // Tại sự kiện chia tiến trình
     pid = fork();
 
     if (pid == 0)
-    {                               // Tiến trình con
-        dup2(fd[1], STDOUT_FILENO); // ghi vào file fd[1]
-        // Đóng phần fd[1] không sử dụng
+    {                               
+        dup2(fd[1], STDOUT_FILENO); 
         close(fd[0]);
-        // Đóng phần fd[2] đã sử dụng xong
         close(fd[1]);
-        // Thực thi lệnh
         if (execvp(args[0], args) < 0)
         {
             printf("\nCan't execute pipe 1...");
@@ -158,7 +136,7 @@ void execArgsPipe(char *args[], char *argsPipe[], int flag)
         exit(1);
     }
     else if (pid > 0)
-    { // chương trình cha
+    { 
 
         pid = fork();
         if (pid == 0)
@@ -195,10 +173,9 @@ void execArgsPipe(char *args[], char *argsPipe[], int flag)
     }
 }
 
-// Ham exec tong quat cho tat ca truong hop
 void exec(char *args[], int len)
 {
-    int flag = 0; // không có &
+    int flag = 0;
     int iPipe = checkPipe(args, len);
     if (iPipe == -1)
     {
@@ -217,8 +194,8 @@ void exec(char *args[], int len)
         argsPipe[i] = NULL;
         if (*argsPipe[i - 1] == '&')
         {
-            flag = 1;               // có &
-            argsPipe[i - 1] = NULL; // xóa dấu & trong lệnh
+            flag = 1;               
+            argsPipe[i - 1] = NULL; 
         }
         args[iPipe] = NULL;
         execArgsPipe(args, argsPipe, flag);
@@ -240,11 +217,11 @@ int main(void)
         int len;
         int res = getInput(input, history);
         if (res == 1)
-        { // thực hiện lệnh
+        { 
             len = parseInput(args, input);
             exec(args, len);
         }
-        else if (res == 0) // exit
+        else if (res == 0) 
         {
             shouldRun = 0;
         }
